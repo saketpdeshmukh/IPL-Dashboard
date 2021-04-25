7ee98deecbbdedb55040057cb89c60c2a56c62e5
haveacupofjava19 <saketdeshmukh95@gmail.com>
2021-04-25 21:07:45 +0530

    Added Home Page and Year Selector in Match Page

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,7 +1,9 @@
 import { React, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { MatchDetailCard } from "../components/MatchDetailCard";
-import { MatchSmallCard } from "../components/MatchSmallCard";
+import { YearSelector } from "../components/YearSelector";
+
+import "./MatchPage.scss";
 
 export const MatchPage = () => {
   const [matches, setMatches] = useState([]);
@@ -15,14 +17,22 @@ export const MatchPage = () => {
       setMatches(data);
     };
     fetchMatches();
-  }, []);
+  }, [teamName, year]);
 
   return (
     <div className="MatchPage">
-      <h1>Match Page</h1>
-      {matches.map((match) => (
-        <MatchDetailCard teamName={teamName} match={match} />
-      ))}
+      <div className="year-selector">
+        <h3>Select Year</h3>
+        <YearSelector teamName={teamName} />
+      </div>
+      <div>
+        <h1 className="page-heading">
+          Matches Played by: {teamName} in Year: {year}
+        </h1>
+        {matches.map((match) => (
+          <MatchDetailCard teamName={teamName} match={match} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,5 +1,5 @@
 import { React, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { MatchDetailCard } from "../components/MatchDetailCard";
 import { MatchSmallCard } from "../components/MatchSmallCard";
 import { PieChart } from "react-minimal-pie-chart";
@@ -10,46 +10,46 @@ export const TeamPage = () => {
   const [team, setTeam] = useState({ matches: [] });
   const { teamName } = useParams();
   useEffect(() => {
-    const fetchMatches = async () => {
+    const fetchTeam = async () => {
       const response = await fetch(`http://localhost:8080/team/${teamName}`);
       const data = await response.json();
       setTeam(data);
     };
-    fetchMatches();
+    fetchTeam();
   }, [teamName]);
 
-  if (!team || !team.teamName) {
-    return <h1>404: Team Not Found</h1>;
-  } else {
-    return (
-      <div className="TeamPage">
-        <div className="team-name-section">
-          <h1 className="team-name">{team.teamName}</h1>
-        </div>
-        <div className="win-loss-section">
-          Wins / Losses
-          <PieChart
-            data={[
-              {
-                title: "Losses",
-                value: team.totalMatchesPlayed - team.totalWins,
-                color: "#a34d5d",
-              },
-              { title: "Wins", value: team.totalWins, color: "#4da375" },
-            ]}
-          />
-        </div>
-        <div className="match-detail-section">
-          <h3> Latest Matches</h3>
-          <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
-        </div>
-        {team.matches.slice(1).map((match) => (
-          <MatchSmallCard teamName={team.teamName} match={match} />
-        ))}
-        <div className="more-link">
-          <a href="#">More ></a>
-        </div>
+  return (
+    <div className="TeamPage">
+      <div className="team-name-section">
+        <h1 className="team-name">{team.teamName}</h1>
       </div>
-    );
-  }
+      <div className="win-loss-section">
+        Wins / Losses
+        <PieChart
+          data={[
+            {
+              title: "Losses",
+              value: team.totalMatchesPlayed - team.totalWins,
+              color: "#a34d5d",
+            },
+            { title: "Wins", value: team.totalWins, color: "#4da375" },
+          ]}
+        />
+      </div>
+      <div className="match-detail-section">
+        <h3> Latest Matches</h3>
+        <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
+      </div>
+      {team.matches.slice(1).map((match) => (
+        <MatchSmallCard teamName={team.teamName} match={match} />
+      ))}
+      <div className="more-link">
+        <Link
+          to={`/teams/${teamName}/matches/${process.env.REACT_APP_DATA_END_YEAR}`}
+        >
+          More >
+        </Link>{" "}
+      </div>
+    </div>
+  );
 };
